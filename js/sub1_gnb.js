abf25ffccdb35468622b41879fddc4f5b4dc4296
won-jiyun <116623509+won-jiyun@users.noreply.github.com>
2022-10-25 15:26:51 +0900

    Add files via upload

---
+++ js/sub1_gnb.js
@@ -0,0 +1,32 @@
+$(function(){
+    //메인
+    $(".gnb>li").mouseenter(function(){
+        $(this).children(".sub").stop().slideDown();
+        $(".box").stop().fadeIn();
+    });//마우스올라갔을때 이벤트
+    $(".gnb>li").mouseleave(function(){
+        $(this).children(".sub").stop().slideUp();
+        $(".box").stop().fadeOut();
+
+
+    
+    });//마우스내려갔을때 이벤트
+/* -- hover를 써서 하는 경우-- */
+
+/* 1. slideDown, slideUp을 쓰는경우
+    $(".gnb_inner>ul>li").hover(function(){
+        $(this).children(".sub").stop().slideDown();
+    },
+    function(){
+        $(this).children(".sub").stop().slideUp();
+    });
+    */
+   /*
+    2. slideToggle사용때
+
+    $(".gnb_inner>ul>li").hover(function(){
+        $(this).children(".sub").stop().slideToggle();
+    })
+
+   */
+});
---
+++ js/sub1_tab.js
@@ -0,0 +1,37 @@
+$(function () {
+    $(".pin_seoul").click(function () {
+      $(".popup1").fadeIn();
+    });
+    $(".popup1 a").click(function () {
+      $(".popup1").fadeOut();
+    });
+
+    $(".pin_paris").click(function () {
+        $(".popup3").fadeIn();
+      });
+      $(".popup3 a").click(function () {
+        $(".popup3").fadeOut();
+      });
+
+      $(".pin_newyork").click(function () {
+        $(".popup2").fadeIn();
+      });
+      $(".popup2 a").click(function () {
+        $(".popup2").fadeOut();
+      });  
+
+
+      $(".pin_brazil").click(function () {
+        $(".popup4").fadeIn();
+      });
+      $(".popup4 a").click(function () {
+        $(".popup4").fadeOut();
+      });
+
+      $(".pin_aus").click(function () {
+        $(".popup5").fadeIn();
+      });
+      $(".popup5 a").click(function () {
+        $(".popup5").fadeOut();
+      });
+  });
